ac69ec1910c1e613b483b9c88f6f810cb1115466
Prashant Chaurasia <prashantchaurasia911@gmail.com>
2019-05-13 16:17:50 +0530

    Basic index.js file for creating basic server
    Run created server on node cli, using command : node index
    Now enter 'localhost:3000' in chrome

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("a")
+const http = require("http");
+const port = 3000;
+
+// create a server object
+
+const server = http.createServer( ( req, res ) => {
+    res.write('Basic Node Server');
+    res.end();
+})
+
+// run node index command and then run localhost:3000 in web browser to see server
+server.listen(port);
